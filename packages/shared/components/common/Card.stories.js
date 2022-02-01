36d711b8b775aecdc89db4aadadf6c1a4e428751
Italo Fontes <italo5123@hotmail.com>
2022-02-01 14:00:23 -0300

    feat: ✨ added storybook to shared module

---
+++ packages/shared/components/common/Card.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Card from './Card';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Common/Card',
+  component: Card,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Card{...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
